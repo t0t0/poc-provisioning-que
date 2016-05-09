51d7ee9f2b3ca2fd7f7aff944cee635991cc5f44
tsDaenor <tomas.vercautter@gmail.com>
2016-05-09 12:23:25 +0200

    readqueues

--- src/readqueue/readqueue.go
+++ src/readqueue/readqueue.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-    "fmt"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/sqs"
 )
+
+
 func readqueue() {
-	
+	svc := sqs.New(session.New())
+
+params := &sqs.ListQueuesInput{
+	QueueNamePrefix: aws.String(""),
+}
+resp, err := svc.ListQueues(params)
+
+if err != nil {
+	// Print the error, cast err to awserr.Error to get the Code and
+	// Message from an error.
+	fmt.Println(err.Error())
+	return
+}
+
+// Pretty-print the response data.
+fmt.Println(resp)
+
 }
