2d84a561247250938403eb8109f279573a96b21f
tsDaenor <tomas.vercautter@gmail.com>
2016-05-10 10:45:26 +0200

    update

--- src/readqueue/readqueue.go
+++ src/readqueue/readqueue.go
@@ -2,10 +2,15 @@ package main
 
 import (
     "fmt"
+    "os"
+    "log"
+
 
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/sqs"
+    "github.com/aws/aws-sdk-go/service/s3"
+    "github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func main() {
@@ -23,7 +28,7 @@ params := &sqs.ReceiveMessageInput{
 	},
 	MaxNumberOfMessages: aws.Int64(10),
 	MessageAttributeNames: []*string{
-		aws.String("ufufyjyv"), // Required
+		aws.String(""), // Required
 		// More values...
 	},
 }
@@ -41,7 +46,55 @@ if err != nil {
 l := resp.Messages
 for i := 0; i < len(l); i ++ {
     v := l[i].Body
+    handler := l[i].ReceiptHandle
     fmt.Println(*v)
+    getFile(*v , *handler)
 }
 
+}
+
+func getFile(key string, handler string) {
+path := "tmp/"
+location := path + key
+file, err := os.Create(location)
+if err != nil {
+ log.Fatal("Failed to create file", err)
+}
+defer file.Close()
+downloader := s3manager.NewDownloader(session.New(&aws.Config{Region:
+aws.String("us-east-1")}))
+numBytes, err := downloader.Download(file,
+ &s3.GetObjectInput{
+ Bucket: aws.String("poc-provision"),
+ Key: aws.String(key),
+ })
+if err != nil {
+ fmt.Println("Failed to download file", err)
+ return
+}
+fmt.Println("Downloaded file", file.Name(), numBytes, "bytes")
+deleteMessage(handler, "https://sqs.us-east-1.amazonaws.com/659527370395/provision-queue")
+	
+}
+
+func deleteMessage(r string, q string) {
+
+	svc := sqs.New(session.New())
+
+	params := &sqs.DeleteMessageInput{
+	QueueUrl:      aws.String(q), // Required
+	ReceiptHandle: aws.String(r), // Required
+}
+resp, err := svc.DeleteMessage(params)
+
+if err != nil {
+	// Print the error, cast err to awserr.Error to get the Code and
+	// Message from an error.
+	fmt.Println(err.Error())
+	return
+}
+
+// Pretty-print the response data.
+fmt.Println(resp)
+	
 }
