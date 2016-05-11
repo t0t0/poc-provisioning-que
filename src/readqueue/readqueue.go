98f01ae2e1b741f72e4c4b106bc6d38d0cf5eb4d
tsDaenor <tomas.vercautter@gmail.com>
2016-05-11 17:17:24 +0200

    global vars

--- src/readqueue/readqueue.go
+++ src/readqueue/readqueue.go
@@ -4,11 +4,15 @@ import (
 // "fmt"
 )
 
+var config
+
 func main() {
+	region := aws.NewConfig().WithRegion(config.Region)
+    session := session.New(region)
 	config := openConfig()
 	sns_arn := generateSnsArn(config)
 	sqs_arn := generateSqsArn(config)
-	createQueue(sqs_arn, sns_arn, config)
+	createQueue(sqs_arn, sns_arn, config , session)
 	// fmt.Println("na createque")
 	// sqs_arn := getQueueArn(*queue.QueueUrl)
 	// fmt.Println("na getqueuearn")
