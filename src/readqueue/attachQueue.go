ed77cc3cc9051a5353592a94a78f4490cf8196ff
tsDaenor <tomas.vercautter@gmail.com>
2016-05-11 09:37:33 +0200

    createqueue

---
+++ src/readqueue/attachQueue.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	//"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func createQueue(queueName string) *sqs.CreateQueueOutput {
+
+	svc := sqs.New(session.New())
+
+	params := &sqs.CreateQueueInput{
+		QueueName: aws.String(queueName),
+		// Attributes: map[string]*string{
+		// 	"Policy": aws.String("SNS-to-SQS"),
+		// }, // Required
+	}
+	resp, err := svc.CreateQueue(params)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return nil
+	}
+
+	// Pretty-print the response data.
+	return resp
+}
+
+func getQueueArn(queueUrl string) string {
+	svc := sqs.New(session.New())
+
+	params := &sqs.GetQueueAttributesInput{
+		QueueUrl: aws.String(queueUrl), // Required
+		AttributeNames: []*string{
+			aws.String("QueueArn"), // Required
+			// More values...
+		},
+	}
+	resp, err := svc.GetQueueAttributes(params)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println(err.Error())
+		return " "
+	}
+
+	// Pretty-print the response data.
+	return *resp.Attributes["QueueArn"]
+}
+
+func getQueuePolicy(queueUrl string) {
+	svc := sqs.New(session.New())
+
+	params := &sqs.GetQueueAttributesInput{
+		QueueUrl: aws.String(queueUrl), // Required
+		AttributeNames: []*string{
+			aws.String("Policy"), // Required
+			// More values...
+		},
+	}
+	resp, err := svc.GetQueueAttributes(params)
+
+	if err != nil {
+		// Print the error, cast err to awserr.Error to get the Code and
+		// Message from an error.
+		fmt.Println("error from getQueuePolicy")
+		fmt.Println(err.Error())
+		return
+	}
+
+	// Pretty-print the response data.
+	fmt.Println(*resp.Attributes["Policy"])
+}
