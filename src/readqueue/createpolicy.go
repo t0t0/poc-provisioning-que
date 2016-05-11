d5dd923317dbdafe3c95ec57b89b3bf6d23c5950
encron <toon.lamberigts@gmail.com>
2016-05-11 11:27:39 +0200

    fixed the json shizzle

--- src/readqueue/createpolicy.go
+++ src/readqueue/createpolicy.go
@@ -5,48 +5,51 @@ import (
 )
 
 type Policy struct {
-	Version   string `json:"Version"`
-	ID        string `json:"Id"`
-	Statement []struct {
-		Effect    string `json:"Effect"`
-		Principal struct {
-			AWS string `json:"AWS"`
-		} `json:"Principal"`
-		Action    string `json:"Action"`
-		Resource  string `json:"Resource"`
-		Condition struct {
-			ArnLike struct {
-				AwsSourceArn string `json:"aws:SourceArn"`
-			} `json:"ArnLike"`
-		} `json:"Condition"`
-	} `json:"Statement"`
+	Version   string    `json:"Version"`
+	ID        string    `json:"Id"`
+	Statement Statement `json:"Statement"`
 }
 
-func createPolicy(sqs_arn string, sns_arn string) *json {
+type Statement struct {
+	Effect    string    `json:"Effect"`
+	Principal Principal `json:"Principal"`
+	Action    string    `json:"Action"`
+	Resource  string    `json:"Resource"`
+	Condition Condition `json:"Condition"`
+}
+
+type Principal struct {
+	AWS string `json:"AWS"`
+}
+
+type Condition struct {
+	ArnLike ArnLike `json:"ArnLike"`
+}
+
+type ArnLike struct {
+	AwsSourceArn string `json:"aws:SourceArn"`
+}
+
+func createPolicy(sqs_arn string, sns_arn string) ([]byte, error) {
 
 	resp := &Policy{
 		Version: "2008-10-17",
-		Id:      "provision",
-		Statement: {
+		ID:      "provision",
+		Statement: *&Statement{
 			Effect: "Allow",
-			Principal: {
+			Principal: *&Principal{
 				AWS: "*",
 			},
 			Action:   "SQS:SendMessage",
 			Resource: sqs_arn,
-			Condition: {
-				ArnLike: {
+			Condition: *&Condition{
+				ArnLike: *&ArnLike{
 					AwsSourceArn: sns_arn,
 				},
 			},
 		},
 	}
 
-	policy, err := json.Marshal(resp)
-	if err != nil {
-		fmt.Printf("Error: %s", err)
-		return
-	}
-	return policy
+	return json.Marshal(resp)
 
 }
